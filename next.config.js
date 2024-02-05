ea2160e47791ef4df0fd5a4f87ab02d3a8bc91e8
abendaamer <ahmed.bendaamer@bca.fr>
2024-02-05 14:45:15 +0100

    Prepare for deployment to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  output: "export",
   basePath: isProd ? "/SwipeSum" : "",
   assetPrefix: isProd ? "/SwipeSum/" : "",
   reactStrictMode: true,
